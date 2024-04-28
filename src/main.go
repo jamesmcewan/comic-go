8f5de769a8ddbd00e424616849f20038ffe92881
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2024-04-28 17:05:09 +0100

    refine

--- src/main.go
+++ src/main.go
@@ -18,20 +18,29 @@ func getEnv(key string) string {
 	return os.Getenv(key)
 }
 
-func main() {
-	apiKey := getEnv("COMICVINE_KEY")
+func getFullUrl(apiKey string) string {
 	url := "https://comicvine.gamespot.com/api/volumes/?api_key="
 	query := "&format=json&sort=name:asc&filter=name:Walking%20Dead"
-	reqUrl := url + apiKey + query
-	res, err := http.Get(reqUrl)
-	if err != nil {
-		fmt.Printf("error %s \n", err)
-	}
 
+	return url + apiKey + query
+}
+
+func getComics(fullUrl string) []byte {
+	res, err := http.Get(fullUrl)
+	if err != nil {
+		fmt.Println("oh no")
+	}
 	resBody, err := io.ReadAll(res.Body)
 	if err != nil {
-		fmt.Printf("error %s \n", err)
+		fmt.Println("oh no")
 	}
 
-	fmt.Printf("%s \n", resBody)
+	return resBody
+}
+
+func main() {
+	apiKey := getEnv("COMICVINE_KEY")
+
+	result := getComics(getFullUrl(apiKey))
+	fmt.Printf("%s \n", result)
 }
