60a19b0b3082e4be0e4c7f0a6bf646a4c8c7ce06
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2024-04-30 21:19:24 +0100

    done

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Comic go
+# Comicvine list comics from a publisher - gopher's version
+
+_This is basically the same programme as the one I wrote in Rust but for fun this one is in Go._
+
+For some reason the comicvine website does not have a way to list all the comics from a publisher. This is a simple cli tool that does that.
+
+Requires a comicvine api key. You can get one by creating an account at https://comicvine.gamespot.com/api/ then add the `COMICVINE_API_KEY` to a `.env` file in the root of the project.
+
+## Usage
+
+Get the publisher id from the comicvine website. For example, the publisher id for Titan Books is `4010-4212`.
+
+```bash
+#Remember  the quotes
+go run ./cmd "4010-4212"
+
+# To save the output to a file
+go run ./cmd "4010-4212" > titan-books.txt
+```
+
--- cmd/main.go
+++ cmd/main.go
@@ -3,26 +3,61 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
 )
 
-type ComicResponse struct {
-	NumberOfPageResults int `json:"number_of_page_results"`
-	Results             []struct {
+type ComicVineResponse struct {
+	Results struct {
 		Aliases string `json:"aliases"`
 		Volumes []struct {
-			Name string `json:"name"`
-			ID   int    `json:"id"`
-			URL  string `json:"site_detail_url"`
+			Name          string `json:"name"`
+			SiteDetailURL string `json:"site_detail_url"`
 		} `json:"volumes"`
 	} `json:"results"`
 }
 
 func main() {
 	apiKey := getEnv("COMICVINE_KEY")
+	publisherId := os.Args[1]
+	fmt.Println("Searching for Publisher ID:", publisherId)
+	fullURL := getFullUrl(apiKey, publisherId)
 
-	result := getComics(getFullUrl(apiKey, "4010-4212"))
+	client := http.Client{}
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
 
-	var comicResponseStruct ComicResponse
-	json.Unmarshal(result, &comicResponseStruct)
-	fmt.Printf("API response %+v\n", comicResponseStruct)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0")
+
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode == http.StatusOK {
+		fmt.Println("Response OK")
+		var comicVineResp ComicVineResponse
+		err := json.NewDecoder(response.Body).Decode(&comicVineResp)
+		if err != nil {
+			fmt.Println("Error decoding JSON:", err)
+			return
+		}
+
+		fmt.Println("Result found")
+		fmt.Println("--------------------------------")
+		fmt.Printf("Name: %v\n\n", comicVineResp.Results.Aliases)
+		fmt.Println("--------------------------------")
+		for _, volume := range comicVineResp.Results.Volumes {
+			fmt.Printf("%v\n", volume.Name)
+			fmt.Printf("%v\n", volume.SiteDetailURL)
+			fmt.Println("--------------------------------")
+		}
+	} else {
+		fmt.Printf("Request failed with status code: %d", response.StatusCode)
+	}
 }
